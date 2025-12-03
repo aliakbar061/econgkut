55e842a22444c23ba1eff31e5ad41a6c14b85f8e
Ali Akbar <aliakbar06110@gmail.com>
2025-12-03 18:26:18 +0700

    Fix dependencies and remove emergentintegrations

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -19,7 +19,7 @@ const LandingPage = () => {
             <div className="w-10 h-10 bg-green-600 rounded-full flex items-center justify-center">
               <Truck className="w-6 h-6 text-white" />
             </div>
-            <span className="text-xl font-bold text-green-800">EcoCollect</span>
+            <span className="text-xl font-bold text-green-800">Econgkut</span>
           </div>
           <Button 
             onClick={handleLogin}
